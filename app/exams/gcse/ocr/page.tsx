f967548d5f429b23242a3d86e4c95491c59ed1ed
Sampath Karthik <87607688+phantostick@users.noreply.github.com>
2026-06-17 14:10:34 +0530

    Update page.tsx

--- app/exams/gcse/ocr/page.tsx
+++ app/exams/gcse/ocr/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { GCSECalculatorClient } from '@/gcse-calculator-client';
+import { GCSECalculatorClient } from '../gcse-calculator-client';
 import { Navbar } from '@/components/navbar';
 import { Footer } from '@/components/footer';
 import {
